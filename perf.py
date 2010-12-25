1fd4a105c287d684e86d9ce8ec15ec2d9fb63fe5
Jeff Lindsay <progrium@gmail.com>
2010-12-24 18:16:54 -0800

    made it a two mode tool that returns the same data on enqueue and dequeue

--- perf.py
+++ perf.py
@@ -16,11 +16,18 @@ import utils
 
 use_hub('zeromq')
 
+task = sys.argv[1]
+nodes = utils.cluster(sys.argv[2])
 ctx = zmq.Context()
 
-nodes = utils.cluster(sys.argv[1])
 
 messages = []
+
+def stopped(count, td):
+    print count
+    print td
+    print 1/(td/count)
+    sys.exit(0)
 
 def enqueuer(n):
     frontend = ctx.socket(zmq.REQ)
@@ -33,29 +40,41 @@ def enqueuer(n):
         messages.append(job)
         #print resp
 
-def worker():
+def dequeuer():
     dispatcher = ctx.socket(zmq.PULL)
     finisher = ctx.socket(zmq.PUB)
     for node in nodes:
         dispatcher.connect('tcp://%s:8000' % node)
         finisher.connect('tcp://%s:9000' % node)
+    timeout = None
+    t1 = None
     while True:
         id, job = dispatcher.recv().split(':', 1)
-        #print job
+        if not timeout is None:
+            timeout.cancel()
+        if not t1:
+            t1 = time.time()
         messages.append(job)
         finisher.send(id)
+        td = time.time() - t1
+        timeout = eventlet.spawn_after(5, stopped, len(messages), td)
+        
         
 
 try:
-    size = 10000
-    eventlet.spawn_n(worker)
-    t1 = time.time()
-    eventlet.spawn_n(enqueuer, size)
-    while len(messages) < size:
-        eventlet.sleep(0.1)
-    td = time.time() - t1
-    print td
-    print 1/(td/(size))
+    if task == 'enqueue':
+        size = int(os.environ.get('MESSAGES', 10000))
+        t1 = time.time()
+        eventlet.spawn_n(enqueuer, size)
+        while len(messages) < size:
+            eventlet.sleep(0.1)
+        td = time.time() - t1
+        stopped(len(messages), td)
+        
+    elif task == 'dequeue':
+        eventlet.spawn_n(dequeuer)
+        while True:
+            eventlet.sleep(0.1)
         
 except (KeyboardInterrupt, SystemExit):
-    print "Exiting."
+    sys.exit(0)
